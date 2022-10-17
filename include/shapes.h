4ade3b9c674a45eaac66fd064362d3a36e15330f
Oskari Kinnunen <okinnune@c2r6p3.hive.fi>
2022-10-17 20:23:18 +0300

    added button drawing

---
+++ include/shapes.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shapes.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okinnune <okinnune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 13:54:56 by okinnune          #+#    #+#             */
+/*   Updated: 2022/10/17 20:08:02 by okinnune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHAPES_H
+# define SHAPES_H
+
+#include "vectors.h"
+
+typedef struct s_rectangle
+{
+	t_point	position;
+	t_point	size;
+}	t_rectangle;
+
+# endif
