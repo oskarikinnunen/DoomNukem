041b3cc70490f9e8082bf4b7ccbfac4444f925d0
Oskari Kinnunen <okinnune@c2r4p10.hive.fi>
2022-10-25 13:33:26 +0300

    removed some sdl object files

--- SDL-release-2.0.8/build/build/SDL_d3dmath.d
+++
@@ -1,9 +0,0 @@
-build/SDL_d3dmath.lo: \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/src/render/SDL_d3dmath.c \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/src/render/../SDL_internal.h \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/src/render/../dynapi/SDL_dynapi.h \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/src/render/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/include/SDL_platform.h \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/include/begin_code.h \
-  /Users/vlaine/vscode/merge_doom/SDL-release-2.0.8/include/close_code.h
