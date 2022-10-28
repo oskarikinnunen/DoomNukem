66809b155cd8c6fd4821ad1c29dd5b8e20c01eb9
Vili-valtteri Laine <vlaine@c3r4p6.hive.fi>
2022-10-28 20:11:02 +0300

    refractored triangle clip function

--- src/game_3d.c
+++ src/game_3d.c
@@ -6,7 +6,7 @@
 /*   By: vlaine <vlaine@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:05:07 by vlaine            #+#    #+#             */
-/*   Updated: 2022/10/27 18:46:08 by vlaine           ###   ########.fr       */
+/*   Updated: 2022/10/28 20:09:30 by vlaine           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,178 +14,105 @@
 #include "inputhelp.h"
 #include "bresenham.h"
 
-#define PRINT_DEBUG 1
-
-static t_quaternion Initquater()
+static void save_clipped_points(float dist[3], t_triangle *tris[3], int points[4])
 {
-	t_vector3 v;
-	t_quaternion q;
-
-	v.x = 0.0f;
-	v.y = 0.0f;
-	v.z = 0.0f;
-	q.v = v;
-	q.w = 1.0f;
-	return (q);
+	if (dist[0] >= 0)
+	{
+		tris[1]->p[points[0]++] = tris[0]->p[0];
+		tris[1]->t[points[1]++] = tris[0]->t[0];
+	}
+	else
+	{
+		tris[2]->p[points[2]++] = tris[0]->p[0];
+		tris[2]->t[points[3]++] = tris[0]->t[0];
+	}
+	if (dist[1] >= 0)
+	{
+		tris[1]->p[points[0]++] = tris[0]->p[1];
+		tris[1]->t[points[1]++] = tris[0]->t[1];
+	}
+	else
+	{
+		tris[2]->p[points[2]++] = tris[0]->p[1];
+		tris[2]->t[points[3]++] = tris[0]->t[1];
+	}
+	if (dist[2] >= 0)
+	{
+		tris[1]->p[points[0]++] = tris[0]->p[2];
+		tris[1]->t[points[1]++] = tris[0]->t[2];
+	}
+	else
+	{
+		tris[2]->p[points[2]++] = tris[0]->p[2];
+		tris[2]->t[points[3]++] = tris[0]->t[2];
+	}
 }
 
-static t_triangle Inittri()
+static int Triangle_ClipAgainstPlane(t_vector3 plane_p, t_vector3 plane_n, t_triangle *in_tri, t_triangle out_tri[2])
 {
-	t_triangle t;
+	t_triangle	tri_inside;
+	t_triangle	tri_outside;
+	float		dist[3];
+	int			points[4];
+	float		t;
 
-	t.p[0] = Initquater();
-	t.p[1] = Initquater();
-	t.p[2] = Initquater();
-	t.clr = CLR_GRAY;
-	return(t);
-}
-
-static int Triangle_ClipAgainstPlane(t_vector3 plane_p, t_vector3 plane_n, t_triangle *in_tri, t_triangle *out_tri1, t_triangle *out_tri2)
-{
-	// Make sure plane normal is indeed normal
+	bzero(points, sizeof(int[4]));
 	plane_n = vector3_normalise(plane_n);
+	dist[0] = vector3_fdist_to_plane(in_tri->p[0].v, plane_n, plane_p);
+	dist[1] = vector3_fdist_to_plane(in_tri->p[1].v, plane_n, plane_p);
+	dist[2] = vector3_fdist_to_plane(in_tri->p[2].v, plane_n, plane_p);
 
-	// Create two temporary storage arrays to classify points either side of plane
-	// If dist1ance sign is positive, point lies on "inside" of plane
-	t_quaternion inside_points[3];  int nInsidePointCount = 0;
-	t_quaternion outside_points[3]; int nOutsidePointCount = 0;
-	t_texture inside_tex[3]; int nInsideTexCount = 0;
-	t_texture outside_tex[3]; int nOutsideTexCount = 0;
-
-	for (int e = 0; e < 3; e++)
+	save_clipped_points(dist, (t_triangle *[3]){in_tri, &tri_inside, &tri_outside}, points);
+	if (points[0] == 0)
+		return 0;
+	if (points[0] == 3)
 	{
-		inside_points[e] = Initquater();
-		outside_points[e] = Initquater();
+		out_tri[0] = *in_tri;
+		return 1;
 	}
-	// Get signed dist1ance of each point in triangle to plane
-	float d0 = vector3_fdist_to_plane(in_tri->p[0].v, plane_n, plane_p);
-	float d1 = vector3_fdist_to_plane(in_tri->p[1].v, plane_n, plane_p);
-	float d2 = vector3_fdist_to_plane(in_tri->p[2].v, plane_n, plane_p);
-
-	if (d0 >= 0)
+	if (points[0] == 1 && points[2] == 2)
 	{
-		inside_points[nInsidePointCount++] = in_tri->p[0];
-		inside_tex[nInsideTexCount++] = in_tri->t[0];
+		out_tri[0].clr = in_tri->clr;
+
+		out_tri[0].p[0] = tri_inside.p[0];
+		out_tri[0].t[0] = tri_inside.t[0];
+
+		out_tri[0].p[1] = quaternion_intersectplane(plane_p, plane_n, tri_inside.p[0], tri_outside.p[0], &t);
+		out_tri[0].t[1].u = t * (tri_outside.t[0].u - tri_inside.t[0].u) + tri_inside.t[0].u;
+		out_tri[0].t[1].v = t * (tri_outside.t[0].v - tri_inside.t[0].v) + tri_inside.t[0].v;
+		out_tri[0].t[1].w = t * (tri_outside.t[0].w - tri_inside.t[0].w) + tri_inside.t[0].w;
+
+		out_tri[0].p[2] = quaternion_intersectplane(plane_p, plane_n, tri_inside.p[0], tri_outside.p[1], &t);
+		out_tri[0].t[2].u = t * (tri_outside.t[1].u - tri_inside.t[0].u) + tri_inside.t[0].u;
+		out_tri[0].t[2].v = t * (tri_outside.t[1].v - tri_inside.t[0].v) + tri_inside.t[0].v;
+		out_tri[0].t[2].w = t * (tri_outside.t[1].w - tri_inside.t[0].w) + tri_inside.t[0].w;
+		return 1;
 	}
-	else
+	if (points[0] == 2 && points[2] == 1)
 	{
-		outside_points[nOutsidePointCount++] = in_tri->p[0];
-		outside_tex[nOutsideTexCount++] = in_tri->t[0];
-	}
-	if (d1 >= 0)
-	{
-		inside_points[nInsidePointCount++] = in_tri->p[1];
-		inside_tex[nInsideTexCount++] = in_tri->t[1];
-	}
-	else
-	{
-		outside_points[nOutsidePointCount++] = in_tri->p[1];
-		outside_tex[nOutsideTexCount++] = in_tri->t[1];
-	}
-	if (d2 >= 0)
-	{
-		inside_points[nInsidePointCount++] = in_tri->p[2];
-		inside_tex[nInsideTexCount++] = in_tri->t[2];
-	}
-	else
-	{
-		outside_points[nOutsidePointCount++] = in_tri->p[2];
-		outside_tex[nOutsideTexCount++] = in_tri->t[2];
-	}
+		out_tri[0].clr = in_tri->clr;
+		out_tri[1].clr = in_tri->clr;
 
-	// Now classify triangle points, and break the input triangle into 
-	// smaller output triangles if required. There are four possible
-	// outcomes...
+		out_tri[0].p[0] = tri_inside.p[0];
+		out_tri[0].p[1] = tri_inside.p[1];
+		out_tri[0].t[0] = tri_inside.t[0];
+		out_tri[0].t[1] = tri_inside.t[1];
 
-	if (nInsidePointCount == 0)
-	{
-		// All points lie on the outside of plane, so clip whole triangle
-		// It ceases to exist
+		out_tri[0].p[2] = quaternion_intersectplane(plane_p, plane_n, tri_inside.p[0], tri_outside.p[0], &t);
+		out_tri[0].t[2].u = t * (tri_outside.t[0].u - tri_inside.t[0].u) + tri_inside.t[0].u;
+		out_tri[0].t[2].v = t * (tri_outside.t[0].v - tri_inside.t[0].v) + tri_inside.t[0].v;
+		out_tri[0].t[2].w = t * (tri_outside.t[0].w - tri_inside.t[0].w) + tri_inside.t[0].w;
 
-		return 0; // No returned triangles are valid
-	}
+		out_tri[1].p[0] = tri_inside.p[1];
+		out_tri[1].t[0] = tri_inside.t[1];
+		out_tri[1].p[1] = out_tri[0].p[2];
+		out_tri[1].t[1] = out_tri[0].t[2];
+		out_tri[1].p[2] = quaternion_intersectplane(plane_p, plane_n, tri_inside.p[1], tri_outside.p[0], &t);
+		out_tri[1].t[2].u = t * (tri_outside.t[0].u - tri_inside.t[1].u) + tri_inside.t[1].u;
+		out_tri[1].t[2].v = t * (tri_outside.t[0].v - tri_inside.t[1].v) + tri_inside.t[1].v;
+		out_tri[1].t[2].w = t * (tri_outside.t[0].w - tri_inside.t[1].w) + tri_inside.t[1].w;
 
-	if (nInsidePointCount == 3)
-	{
-		// All points lie on the inside of plane, so do nothing
-		// and allow the triangle to simply pass through
-		*out_tri1 = *in_tri;
-
-		return 1; // Just the one returned original triangle is valid
-	}
-
-	if (nInsidePointCount == 1 && nOutsidePointCount == 2)
-	{
-
-		// Triangle should be clipped. As two points lie outside
-		// the plane, the triangle simply becomes a smaller triangle
-
-		// Copy appearance info to new triangle
-		out_tri1->clr = in_tri->clr;
-		//out_tri1->sym = in_tri->sym;
-
-		// The inside point is valid, so keep that...
-		out_tri1->p[0] = inside_points[0];
-		out_tri1->t[0] = inside_tex[0];
-
-		// but the two new points are at the locations where the 
-		// original sides of the triangle (lines) intersect with the plane
-		float t;
-		out_tri1->p[1] = quaternion_intersectplane(plane_p, plane_n, inside_points[0], outside_points[0], &t);
-		out_tri1->t[1].u = t * (outside_tex[0].u - inside_tex[0].u) + inside_tex[0].u;
-		out_tri1->t[1].v = t * (outside_tex[0].v - inside_tex[0].v) + inside_tex[0].v;
-		out_tri1->t[1].w = t * (outside_tex[0].w - inside_tex[0].w) + inside_tex[0].w;
-
-		out_tri1->p[2] = quaternion_intersectplane(plane_p, plane_n, inside_points[0], outside_points[1], &t);
-		out_tri1->t[2].u = t * (outside_tex[1].u - inside_tex[0].u) + inside_tex[0].u;
-		out_tri1->t[2].v = t * (outside_tex[1].v - inside_tex[0].v) + inside_tex[0].v;
-		out_tri1->t[2].w = t * (outside_tex[1].w - inside_tex[0].w) + inside_tex[0].w;
-		return 1; // Return the newly formed single triangle
-	}
-
-	if (nInsidePointCount == 2 && nOutsidePointCount == 1)
-	{
-		// Triangle should be clipped. As two points lie inside the plane,
-		// the clipped triangle becomes a "quad". Fortunately, we can
-		// represent a quad with two new triangles
-
-		// Copy appearance info to new triangles
-		//out_tri1.col =  in_tri.col;
-		//out_tri1.sym = in_tri.sym;
-
-		//out_tri2.col =  in_tri.col;
-		//out_tri2.sym = in_tri.sym;
-
-		out_tri1->clr = in_tri->clr;
-		out_tri2->clr = in_tri->clr;
-
-		// The first triangle consists of the two inside points and a new
-		// point determined by the location where one side of the triangle
-		// intersects with the plane
-		out_tri1->p[0] = inside_points[0];
-		out_tri1->p[1] = inside_points[1];
-		out_tri1->t[0] = inside_tex[0];
-		out_tri1->t[1] = inside_tex[1];
-
-		float t;
-		out_tri1->p[2] = quaternion_intersectplane(plane_p, plane_n, inside_points[0], outside_points[0], &t);
-		out_tri1->t[2].u = t * (outside_tex[0].u - inside_tex[0].u) + inside_tex[0].u;
-		out_tri1->t[2].v = t * (outside_tex[0].v - inside_tex[0].v) + inside_tex[0].v;
-		out_tri1->t[2].w = t * (outside_tex[0].w - inside_tex[0].w) + inside_tex[0].w;
-
-		// The second triangle is composed of one of he inside points, a
-		// new point determined by the intersection of the other side of the 
-		// triangle and the plane, and the newly created point above
-		out_tri2->p[0] = inside_points[1];
-		out_tri2->t[0] = inside_tex[1];
-		out_tri2->p[1] = out_tri1->p[2];
-		out_tri2->t[1] = out_tri1->t[2];
-		out_tri2->p[2] = quaternion_intersectplane(plane_p, plane_n, inside_points[1], outside_points[0], &t);
-		out_tri2->t[2].u = t * (outside_tex[0].u - inside_tex[1].u) + inside_tex[1].u;
-		out_tri2->t[2].v = t * (outside_tex[0].v - inside_tex[1].v) + inside_tex[1].v;
-		out_tri2->t[2].w = t * (outside_tex[0].w - inside_tex[1].w) + inside_tex[1].w;
-
-		return 2; // Return two newly formed triangles which form a quad
+		return 2;
 	}
 	return(0);
 }
@@ -206,12 +133,10 @@ static void clipped(int count, t_triangle *triangles_calc, t_sdlcontext sdl, t_i
 	int end = 0;
 
 	t_triangle triangles[200];
-
+	t_triangle clipped[2];
 
 	while (i < count && 1)
 	{
-		t_triangle clipped[2];
-
 		triangles[end++] = triangles_calc[i];
 		int nnewtriangles = 1;
 		for (int p = 0; p < 4; p++)
@@ -219,17 +144,15 @@ static void clipped(int count, t_triangle *triangles_calc, t_sdlcontext sdl, t_i
 			int ntristoadd = 0;
 			while (nnewtriangles > 0)
 			{
-				clipped[0] = Inittri();
-				clipped[1] = Inittri();
-				t_triangle test = Inittri();
+				t_triangle test;
 				test = triangles[start++];
 				nnewtriangles--;
 				switch (p)
 				{
-				case 0: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, 0.0f, 0.0f}, (t_vector3){0.0f, 1.0f, 0.0f}, &test, &clipped[0], &clipped[1]); break;
-				case 1: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, (float)sdl.window_h - 1.0f, 0.0f}, (t_vector3){0.0f, -1.0f, 0.0f}, &test, &clipped[0], &clipped[1]); break;
-				case 2: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, 0.0f, 0.0f}, (t_vector3){1.0f, 0.0f, 0.0f}, &test, &clipped[0], &clipped[1]); break;
-				case 3: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){(float)sdl.window_w - 1.0f, 0.0f, 0.0f}, (t_vector3){-1.0f, 0.0f, 0.0f}, &test, &clipped[0], &clipped[1]); break;
+				case 0: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, 0.0f, 0.0f}, (t_vector3){0.0f, 1.0f, 0.0f}, &test, clipped); break;
+				case 1: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, (float)sdl.window_h - 1.0f, 0.0f}, (t_vector3){0.0f, -1.0f, 0.0f}, &test, clipped); break;
+				case 2: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){0.0f, 0.0f, 0.0f}, (t_vector3){1.0f, 0.0f, 0.0f}, &test, clipped); break;
+				case 3: ntristoadd = Triangle_ClipAgainstPlane((t_vector3){(float)sdl.window_w - 1.0f, 0.0f, 0.0f}, (t_vector3){-1.0f, 0.0f, 0.0f}, &test, clipped); break;
 				}
 				for (int w = 0; w < ntristoadd; w++)
 				{
@@ -247,7 +170,7 @@ static void clipped(int count, t_triangle *triangles_calc, t_sdlcontext sdl, t_i
 
 static t_triangle transform_calc(t_mat4x4 matworld, t_triangle triangles)
 {
-	t_triangle tritransformed = Inittri();
+	t_triangle tritransformed;
 
 	tritransformed.p[0] = quaternion_mul_matrix(matworld, triangles.p[0]);
 	tritransformed.p[1] = quaternion_mul_matrix(matworld, triangles.p[1]);
@@ -274,7 +197,7 @@ static t_vector3 normal_calc(t_triangle tritransformed)
 
 static int clippedtriangles(t_triangle tritransformed, t_mat4x4 matview, t_triangle *clipped)
 {
-	t_triangle triviewed = Inittri();
+	t_triangle triviewed;
 
 	triviewed.p[0] = quaternion_mul_matrix(matview, tritransformed.p[0]);
 	triviewed.p[1] = quaternion_mul_matrix(matview, tritransformed.p[1]);
@@ -283,14 +206,14 @@ static int clippedtriangles(t_triangle tritransformed, t_mat4x4 matview, t_trian
 	triviewed.t[1] = tritransformed.t[1];
 	triviewed.t[2] = tritransformed.t[2];
 	return (Triangle_ClipAgainstPlane(
-	(t_vector3){0.0f, 0.0f, 0.1f},
+	(t_vector3){0.0f, 0.0f, 0.2f},
 	(t_vector3){0.0f, 0.0f, 1.0f},
-	&triviewed, &(clipped[0]), &(clipped[1])));
+	&triviewed, clipped));
 }
 
 static t_triangle triangle_to_screenspace(t_mat4x4 matproj, t_triangle clipped, t_sdlcontext sdl)
 {
-	t_triangle triprojected = Inittri();
+	t_triangle triprojected;
 
 	triprojected.p[0] = quaternion_mul_matrix(matproj, clipped.p[0]);
 	triprojected.p[1] = quaternion_mul_matrix(matproj, clipped.p[1]);
@@ -368,7 +291,7 @@ void engine3d(t_sdlcontext sdl, t_game game)
 		}
 		i++;
 	}
-	if (debug_img.length == 0)
+	if (debug_img.length == 0) // TODO: Get image from obj file, when obj handling is done, currently only for debugging textures
 		debug_img = get_image_by_name(sdl, "");
 	clipped(count, triangles_calc, sdl, debug_img);
 }
