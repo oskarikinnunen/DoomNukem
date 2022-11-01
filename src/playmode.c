ec071e8dd2fcd8f42528a2240f9d506d81d356ff
Oskari Kinnunen <eino.oskari.kinnunen@gmail.com>
2022-11-01 16:34:43 +0200

    fixed merge

--- src/playmode.c
+++ src/playmode.c
@@ -66,29 +66,6 @@ static int handleinput(t_game *game)
 	return(game_continue);
 }
 
-static t_render init_render(t_sdlcontext sdl)
-{
-	t_render	render;
-
-	bzero(&render, sizeof(t_render));
-	render.matworld = matrix_makeidentity();
-	render.matproj = matrix_makeprojection(90.0f, (float)sdl.window_h / (float)sdl.window_w, 2.0f, 1000.0f);
-	render.calc_triangles = malloc(sizeof(t_triangle) * 10000);
-	render.draw_triangles = malloc(sizeof(t_triangle) * 10000);
-	render.q = malloc(sizeof(t_quaternion) * 10000); //TODO: should be multiplied by the largest obj vertex count
-	render.debug_img = get_image_by_name(sdl, "");
-	/*temp testing render loop with .bot .wall .item*/
-	t_entity	temp;
-
-	bzero(&temp, sizeof(t_entity));
-	temp.obj = &sdl.objects[1];
-	temp.transform.scale = vector3_one();
-	list_push(&render.listbot, &temp, sizeof(t_bot));
-	temp.obj = &sdl.objects[0];
-	list_push(&render.listitem, &temp, sizeof(t_item));
-	return(render);
-}
-
 static void update_render(t_render *render, t_player player)
 {
 	render->lookdir = player.lookdir;
