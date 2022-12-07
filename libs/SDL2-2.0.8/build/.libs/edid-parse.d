51c43e62634ef03a2643a9cb6496beddd3e35da2
Rene Aho <raho@c1r1p6.hive.fi>
2022-12-07 19:08:08 +0200

    remove compiled libs

--- libs/SDL2-2.0.8/build/.libs/edid-parse.d
+++
@@ -1,8 +0,0 @@
-build/edid-parse.lo: \
- /home/oskari/projects/DoomNukem/libs/SDL2-2.0.8/src/video/x11/edid-parse.c \
- /home/oskari/projects/DoomNukem/libs/SDL2-2.0.8/src/video/x11/../../SDL_internal.h \
- /home/oskari/projects/DoomNukem/libs/SDL2-2.0.8/src/video/x11/../../dynapi/SDL_dynapi.h \
- /home/oskari/projects/DoomNukem/libs/SDL2-2.0.8/src/video/x11/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_stdinc.h include/SDL_config.h \
- /home/oskari/projects/DoomNukem/libs/SDL2-2.0.8/src/video/x11/edid.h
