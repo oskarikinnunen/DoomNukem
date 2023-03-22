41d28fb6257481a50138c302f20b18c97650dc00
Vili-valtteri Laine <vlaine@c2r5p1.hive.fi>
2023-03-22 18:07:52 +0200

    working version

--- src/thread/thread_lighting.c
+++ src/thread/thread_lighting.c
@@ -5,7 +5,7 @@ void	set_light_thread_struct(
 {
 	t_test	*ptr;
 
-	if (ent->status != es_active && ent->component.type != COMP_LIGHT)
+	if (ent->status != es_active || ent->component.type != COMP_LIGHT)
 		return ;
 	ptr = &(((t_test *)thread->structs)[thread->count++]);
 	ptr->entity = ent;
@@ -16,7 +16,7 @@ void	set_light_thread_struct(
 
 void	set_lighting_texture_struct(t_thread *thread, struct s_entity *ent)
 {
-	if	(ent->status == es_active && ent->map != NULL)
+	if	(ent->status == es_active || ent->map != NULL)
 	{
 		(((t_entity *)thread->structs)[thread->count++]) = *ent;
 		if (thread->count == THREAD)
