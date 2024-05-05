1fba67809a5beaa50f92d496d211258ee1a36661
Vili-Valtteri Laine <laine.vili.valtteri@gmail.com>
2024-05-05 07:57:48 -0700

    fixed crash

--- src/tga_parser/image_data.cpp
+++ src/tga_parser/image_data.cpp
@@ -92,7 +92,7 @@ static void	parse_bottom_right(int fd, char *filename, t_tga *tga)
 void	save_image_data(int fd, char *filename, t_tga *tga)
 {
 	tga->image_data.pixels = \
-			(uint32_t*)(sizeof(uint32_t) * tga->image_data.size);
+			(uint32_t*)prot_memalloc(sizeof(uint32_t) * tga->image_data.size);
 	if (tga->pixel_order.top && tga->pixel_order.left)
 		parse_top_left(fd, filename, tga);
 	else if (tga->pixel_order.top && tga->pixel_order.right)
